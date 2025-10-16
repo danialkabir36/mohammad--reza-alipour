ab9bdfa693fe471e7b72375349af36bb197199eb
mohammad reza alipour <danialkabir36@gmail.com>
2025-10-16 12:38:27 -0400

    a
    sa

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,62 @@
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/Design uten navn.png";
+import themeIcon from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
+
+
+const Hero = () => {
+  return (
+    <section id="hero" className={styles.container} >
+      <div className={styles.colorModeContainer}>
+        <img src={heroImg} 
+        className={styles.hero}
+        alt="Profile picture of Mohammad reza alipour" />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className="styles info">
+        <h1>
+            Mohammad Reza  
+            <br />
+            alipour
+        </h1>
+        <H2>
+            Full-stack Developer
+        </H2>
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+         <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
