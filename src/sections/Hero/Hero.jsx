a55ee31928d567b08f7072d1eb20cbbbd5a4f797
mohammad reza alipour <danialkabir36@gmail.com>
2025-10-16 13:39:34 -0400

    Add initial styles and structure for the application, including theme support and responsive design

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,6 +1,7 @@
-import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/Design uten navn.png";
-import themeIcon from '../../assets/sun.svg';
+
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/Design uten navn.png';
+import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
 import twitterDark from '../../assets/twitter-dark.svg';
@@ -11,15 +12,22 @@ import linkedinDark from '../../assets/linkedin-dark.svg';
 import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
 
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
-const Hero = () => {
   return (
-    <section id="hero" className={styles.container} >
+    <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
-        <img src={heroImg} 
-        className={styles.hero}
-        alt="Profile picture of Mohammad reza alipour" />
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Harris Johnsen"
+        />
         <img
           className={styles.colorMode}
           src={themeIcon}
@@ -27,15 +35,13 @@ const Hero = () => {
           onClick={toggleTheme}
         />
       </div>
-      <div className="styles info">
+      <div className={styles.info}>
         <h1>
-            Mohammad Reza  
-            <br />
-            alipour
+          Harris
+          <br />
+          Johnsen
         </h1>
-        <H2>
-            Full-stack Developer
-        </H2>
+        <h2>Frontend Developer</h2>
         <span>
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
@@ -47,7 +53,7 @@ const Hero = () => {
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
-         <p className={styles.description}>
+        <p className={styles.description}>
           With a passion for developing modern React web apps for commercial
           businesses.
         </p>
@@ -57,6 +63,6 @@ const Hero = () => {
       </div>
     </section>
   );
-};
+}
 
 export default Hero;
